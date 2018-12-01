ed12247a7002cb7c39d3c28fb1a0fb9ab59f104f
Loran425 <loran425@gmail.com>
2018-12-01 17:51:35 -0600

    Challenge 2 Solution
    Includes timing utility to monitor running script

---
+++ Challenge 2/main.py
@@ -0,0 +1,21 @@
+from Utils import timing
+
+def main():
+    sum = 0
+    iter_counter = 0
+    past_sums = []
+    modifiers = [int(x) for x in open('./input.txt', mode='r').readlines()]
+    while True:
+        running_time = timing.time() - timing.start
+        print(f'[{running_time:9.5f}] Iteration {iter_counter:3}, past_sums length: {len(past_sums):7}')
+        for mod in modifiers:
+            sum += mod
+            if sum in past_sums:
+                return sum
+            else:
+                past_sums.append(sum)
+        iter_counter += 1
+
+
+if __name__ == '__main__':
+    print(main())
