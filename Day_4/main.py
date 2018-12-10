fc84ad6e9dc17b2a5fb8d710c75d2dcd1287e423
Loran425 <Loran425@gmail.com>
2018-12-09 23:08:13 -0600

    Code formatter run

--- Day_4/main.py
+++ Day_4/main.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 
 
 def str2ts(string):
-    pattern = '[0-9]+'
+    pattern = "[0-9]+"
     matches = [int(match) for match in re.findall(pattern, string)]
     return datetime.datetime(*matches)
 
@@ -17,14 +17,15 @@ def main():
     event_log = []
     with open("./input.txt", mode="r") as input:
         for line in input:
-            datestamp, event = line.split(']')
+            datestamp, event = line.split("]")
             event_log.append((str2ts(datestamp), event))
     event_log.sort(key=lambda x: x[0])  # Sort by datestamp
 
     guards = defaultdict(list)
     for event in event_log:
-        if re.search('#[0-9]+', event[1]):  # Check for a guard ID ('#' followed by any number of digits)
-            guard = int(re.search('[0-9]+', event[1]).group(0))  # Extract the ID
+        # Check for a guard ID ('#' followed by any number of digits)
+        if re.search("#[0-9]+", event[1]):
+            guard = int(re.search("[0-9]+", event[1]).group(0))  # Extract the ID
         else:
             guards[guard].append(event[0])
 
@@ -44,15 +45,17 @@ def main():
     target_guard = sorted(asleep, key=lambda x: asleep[x], reverse=True)[0]
     events = guards[target_guard]
 
-    minutes = [0]*60
+    minutes = [0] * 60
 
     for start_time, end_time in zip(events[::2], events[1::2]):
-        for minute in range(start_time.minute, end_time.minute+1):
+        for minute in range(start_time.minute, end_time.minute + 1):
             minutes[minute] += 1
 
     target_minute = minutes.index(max(minutes))
 
-    print(f'Target 1: Guard #{target_guard} @ minute {target_minute}, {target_guard*target_minute}\n')
+    print(
+        f"Target 1: Guard #{target_guard} @ minute {target_minute}, {target_guard*target_minute}\n"
+    )
 
     ###########################################################################
     # Strategy #2
@@ -61,7 +64,7 @@ def main():
 
     target_asleep = dict()
     for guard in guards:
-        minutes = [0]*60
+        minutes = [0] * 60
         events = guards[guard]
         for start_time, end_time in zip(events[::2], events[1::2]):
             for minute in range(start_time.minute, end_time.minute + 1):
@@ -69,7 +72,9 @@ def main():
         target_asleep[guard] = (minutes.index(max(minutes)), max(minutes))
 
     target = sorted(target_asleep, key=lambda x: target_asleep[x][1], reverse=True)[0]
-    print(f'Target 2: Guard #{target} @ minute {target_asleep[target][0]}, {target*target_asleep[target][0]}')
+    print(
+        f"Target 2: Guard #{target} @ minute {target_asleep[target][0]}, {target*target_asleep[target][0]}"
+    )
 
 
 if __name__ == "__main__":
--- Day_5/main.py
+++ Day_5/main.py
@@ -3,9 +3,9 @@ from string import ascii_lowercase
 
 def collapse(string):
     """ Collapses a polymer string according to the challenge details"""
-    output = ''
+    output = ""
     for char in string:
-        prev = '' if not output else output[-1]
+        prev = "" if not output else output[-1]
         if char.lower() == prev.lower() and char != prev:
             output = output[:-1]
         else:
@@ -14,7 +14,7 @@ def collapse(string):
 
 
 def main():
-    with open('./input.txt', mode='r') as f:
+    with open("./input.txt", mode="r") as f:
         data = f.readline().strip()
 
     ###########################################################################
@@ -29,13 +29,13 @@ def main():
     results = dict()
     for char in ascii_lowercase:
         modified_data = data
-        modified_data = modified_data.replace(char, '')
-        modified_data = modified_data.replace(char.upper(), '')
+        modified_data = modified_data.replace(char, "")
+        modified_data = modified_data.replace(char.upper(), "")
 
         results[char] = len(collapse(modified_data))
     winner = sorted(results, key=lambda x: results[x])[0]
     print(f"Best collapse with character '{winner}' final size: {results[winner]}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
